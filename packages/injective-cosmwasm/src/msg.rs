578d88b30d465c60f455871a5ead02c4eda25338
mankenavenkatesh <mankenavenkatesh@gmail.com>
2022-07-09 12:02:12 +0530

    Bindings for invoking Mint and Burn of Token Factory

--- packages/injective-cosmwasm/src/msg.rs
+++ packages/injective-cosmwasm/src/msg.rs
@@ -80,7 +80,22 @@ pub enum InjectiveMsg {
         spot_orders_to_create: Vec<SpotOrder>,
         derivative_orders_to_create: Vec<DerivativeOrder>,
     },
+    /// Contracts can mint native tokens for an existing factory denom
+    /// that they are the admin of.
+    Mint {
+        sender: String,
+        amount: Coin,
+        mint_to: String,
+    },
+    /// Contracts can burn native tokens for an existing factory denom
+    /// that they are the admin of.
+    /// Currently, the burn from address must be the admin contract.
+    Burn {
+        sender: String,
+        amount: Coin,
+    },
 }
+
 
 pub fn create_deposit_msg(sender: String, subaccount_id: String, amount: Coin) -> CosmosMsg<InjectiveMsgWrapper> {
     InjectiveMsgWrapper {
@@ -225,6 +240,35 @@ pub fn create_batch_update_orders_msg(
             derivative_orders_to_cancel,
             spot_orders_to_create,
             derivative_orders_to_create,
+        },
+    }
+    .into()
+}
+
+
+pub fn create_mint_tokens_msg(sender: String, amount: Coin, mint_to:String) -> CosmosMsg<InjectiveMsgWrapper> {
+    InjectiveMsgWrapper {
+        route: InjectiveRoute::tokenfactory,
+        msg_data: InjectiveMsg::Mint {
+            sender,
+            amount,
+            mint_to,
+        },
+    }
+    .into()
+}
+
+pub fn create_burn_tokens_msg(
+    sender: String,
+    amount: Coin,
+    _burn_from_address: String,
+) -> CosmosMsg<InjectiveMsgWrapper> {
+
+    InjectiveMsgWrapper {
+        route: InjectiveRoute::tokenfactory,
+        msg_data: InjectiveMsg::Burn {
+            sender,     
+            amount,
         },
     }
     .into()
--- packages/injective-cosmwasm/src/route.rs
+++ packages/injective-cosmwasm/src/route.rs
@@ -8,6 +8,7 @@ pub enum InjectiveRoute {
     // Auction,
     Exchange,
     // Insurance,
+    TokenFactory,
     Oracle,
     // Peggy,
 }
