ddebca8e59f254fee77b117e8cd7d67c4f2d8806
Dan Lynch <pyramation@gmail.com>
2022-10-01 14:19:51 -0500

    registry schemas

--- contracts/registry/examples/schema.rs
+++ contracts/registry/examples/schema.rs
@@ -1,15 +1,25 @@
 use std::env::current_dir;
 use std::fs::create_dir_all;
 
-use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
+use cosmwasm_schema::{export_schema, export_schema_with_title, remove_schemas, schema_for};
 
-use registry::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use registry::msg::{
+    ExecuteMsg,
+    InstantiateMsg,
+    QueryMsg,
+    ContractsResponse,
+    ContractResponse
+};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
     out_dir.push("schema");
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
+
+    export_schema_with_title(&schema_for!(ContractResponse), &out_dir, "GetContractResponse");
+    export_schema_with_title(&schema_for!(ContractsResponse), &out_dir, "GetContractsResponse");
+    export_schema_with_title(&schema_for!(ContractsResponse), &out_dir, "GetActiveContractsResponse");
 
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
