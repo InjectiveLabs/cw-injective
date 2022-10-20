17db7e509e50173d11c50f44bbee228e90bf767d
Mauro Lacy <mauro@lacy.com.es>
2022-10-20 07:42:47 +0200

    Implement IndexedMap for contracts
    Add MultiIndex for contract execution state

--- contracts/registry/src/state.rs
+++ contracts/registry/src/state.rs
@@ -2,7 +2,7 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::Addr;
-use cw_storage_plus::Map;
+use cw_storage_plus::{Index, IndexList, IndexedMap, MultiIndex};
 
 // Contract struct defines begin blocker contract execution params.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
@@ -12,4 +12,32 @@ pub struct CONTRACT {
     pub is_executable: bool,
 }
 
-pub const CONTRACTS: Map<&Addr, CONTRACT> = Map::new("contracts");
+// pub const CONTRACTS: Map<&Addr, CONTRACT> = Map::new("contracts");
+
+pub struct ContractIndexes<'a> {
+    pub active: MultiIndex<'a, u8, CONTRACT, Addr>,
+}
+
+impl<'a> IndexList<CONTRACT> for ContractIndexes<'a> {
+    fn get_indexes(&'_ self) -> Box<dyn Iterator<Item = &'_ dyn Index<CONTRACT>> + '_> {
+        let v: Vec<&dyn Index<CONTRACT>> = vec![&self.active];
+        Box::new(v.into_iter())
+    }
+}
+
+pub fn contracts<'a>() -> IndexedMap<'a, &'a Addr, CONTRACT, ContractIndexes<'a>> {
+    let indexes = ContractIndexes {
+        active: MultiIndex::new(
+            |_, c: &CONTRACT| {
+                if c.is_executable {
+                    1u8
+                } else {
+                    0u8
+                }
+            },
+            "contracts",
+            "contracts__active",
+        ),
+    };
+    IndexedMap::new("contracts", indexes)
+}
