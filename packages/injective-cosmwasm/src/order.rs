8ac3a5759062e4b045d13a5ed72639ee6a5449c0
Markus Waas <mail@markuswaas.com>
2023-02-20 13:52:54 +1300

    fix: add fix for order type serialization

--- packages/injective-cosmwasm/src/order.rs
+++ packages/injective-cosmwasm/src/order.rs
@@ -2,10 +2,11 @@ use cosmwasm_std::Addr;
 use injective_math::FPDecimal;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use serde_repr::Serialize_repr;
 
 use crate::{MarketId, SubaccountId};
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[derive(Serialize_repr, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[repr(u8)]
 pub enum OrderType {
     Undefined = 0,
@@ -37,4 +38,15 @@ pub trait GenericOrder {
     fn get_order_type(&self) -> &OrderType;
     fn get_order_info(&self) -> &OrderInfo;
     fn get_trigger_price(&self) -> Option<FPDecimal>;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::OrderType;
+
+    #[test]
+    fn order_type_serialization() {
+        let types = vec![OrderType::Undefined, OrderType::Buy, OrderType::SellPo, OrderType::SellAtomic];
+        assert_eq!(serde_json_wasm::to_string(&types).unwrap(), "[0,1,8,10]");
+    }
 }
