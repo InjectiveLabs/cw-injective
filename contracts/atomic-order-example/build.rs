fd2a630ddb8cd474c688c208cc7093aa07342d56
Mauro Lacy <mauro@lacy.com.es>
2023-02-24 10:06:07 +0100

    Add trade_data protobuf module

--- contracts/atomic-order-example/build.rs
+++ contracts/atomic-order-example/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     protobuf_codegen_pure::Codegen::new()
         .out_dir("src/proto")
-        .inputs(&["proto/order_reply.proto"])
+        .inputs(&["proto/order_reply.proto", "proto/trade_data.proto"])
         .include("proto")
         .run()
         .expect("Protobuf codegen failed.");
--- contracts/atomic-order-example/src/proto.rs
+++ contracts/atomic-order-example/src/proto.rs
@@ -1,3 +1,5 @@
 pub mod order_reply;
+pub mod trade_data;
 
 pub use order_reply::MsgOrderResponse;
+pub use trade_data::MsgTradeData;
