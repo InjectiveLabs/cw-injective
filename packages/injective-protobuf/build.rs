eff50f0569d9ae4b1b1b6382ebe30c0fe98c5bd1
Mauro Lacy <mauro@lacy.com.es>
2023-03-06 11:19:41 +0100

    Add injective-protobuf package

---
+++ packages/injective-protobuf/build.rs
@@ -0,0 +1,20 @@
+use protobuf_codegen_pure::Customize;
+
+fn main() {
+    let mut customizer = Customize::default();
+    customizer.gen_mod_rs = Some(true);
+
+    protobuf_codegen_pure::Codegen::new()
+        .out_dir("src/proto")
+        .inputs([
+            "third_party/proto/cosmos/base/v1beta1/coin.proto",
+            "proto/injective/exchange/v1beta1/exchange.proto",
+            "proto/injective/exchange/v1beta1/tx.proto",
+            "proto/injective/oracle/v1beta1/oracle.proto",
+            "third_party/proto/cosmos/distribution/v1beta1/distribution.proto",
+        ])
+        .includes(["proto", "third_party/proto"])
+        .customize(customizer)
+        .run()
+        .expect("Protobuf codegen failed.");
+}
