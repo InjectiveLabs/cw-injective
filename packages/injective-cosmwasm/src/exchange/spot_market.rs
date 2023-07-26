bab198f1135e7c7df9d82329d1e5e37940f6c6fe
Markus Waas <mail@markuswaas.com>
2023-07-26 12:04:33 +0200

    chore: improve calculation function

--- packages/injective-cosmwasm/src/exchange/spot_market.rs
+++ packages/injective-cosmwasm/src/exchange/spot_market.rs
@@ -1,4 +1,5 @@
 use crate::exchange::types::MarketId;
+use cosmwasm_std::StdResult;
 use injective_math::FPDecimal;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
@@ -22,13 +23,13 @@ pub struct SpotMarket {
     pub min_quantity_tick_size: FPDecimal,
 }
 
-pub fn calculate_spot_market_id(base_denom: String, quote_denom: String) -> Vec<u8> {
+pub fn calculate_spot_market_id(base_denom: String, quote_denom: String) -> StdResult<MarketId> {
     let mut hasher = Keccak::new_keccak256();
     hasher.update((base_denom + &quote_denom).as_bytes());
     let mut res = [0u8; 32];
     hasher.finalize(&mut res);
 
-    res.to_vec()
+    MarketId::new(format!("0x{}", hex::encode(res)))
 }
 
 #[cfg(test)]
@@ -39,8 +40,13 @@ mod tests {
     fn test_new_spot_market_id() {
         let base = "inj".to_string();
         let quote = "peggy0xdAC17F958D2ee523a2206206994597C13D831ec7".to_string();
-        let result = calculate_spot_market_id(base, quote);
-        let expected_result = hex::decode("a508cb32923323679f29a032c70342c147c17d0145625922b0ef22e955c844c0").unwrap();
-        assert_eq!(result, expected_result, "calculate_spot_market_id did not produce the expected hash");
+        let result = calculate_spot_market_id(base, quote).unwrap();
+        let expected_result = "0xa508cb32923323679f29a032c70342c147c17d0145625922b0ef22e955c844c0";
+
+        assert_eq!(
+            result.as_str(),
+            expected_result,
+            "calculate_spot_market_id did not produce the expected hash"
+        );
     }
 }
