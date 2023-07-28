d9882c4fc09f169e2b6c30702458796df5a15f3f
Mauro Lacy <mauro@lacy.com.es>
2023-07-28 13:43:57 +0200

    Remove extra / unused exported dir

--- packages/injective-protobuf/scripts/protoexport.sh
+++ packages/injective-protobuf/scripts/protoexport.sh
@@ -4,5 +4,7 @@ set -eo pipefail
 echo "Exporting gogo proto files"
 echo "Injective"
 buf export --exclude-imports https://github.com/InjectiveLabs/sdk-go.git --output ./
+# Remove spurious extra exported dir
+rm -rf ./exchange
 echo "Cosmos"
 buf export https://github.com/cosmos/cosmos-sdk.git --output ./third_party/proto
