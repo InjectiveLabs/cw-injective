ae2b9697644098a280c8d51b1b52b8a1ff4d13db
Mauro Lacy <mauro@lacy.com.es>
2023-07-28 13:38:22 +0200

    Export injective proto files (excluding imports)

--- packages/injective-protobuf/scripts/protoexport.sh
+++ packages/injective-protobuf/scripts/protoexport.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 echo "Exporting gogo proto files"
-#echo "Injective"
-#buf export https://github.com/InjectiveLabs/sdk-go.git --output ./proto/injective
+echo "Injective"
+buf export --exclude-imports https://github.com/InjectiveLabs/sdk-go.git --output ./
 echo "Cosmos"
 buf export https://github.com/cosmos/cosmos-sdk.git --output ./third_party/proto
