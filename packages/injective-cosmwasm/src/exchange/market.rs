b69ce966abe1c6110942a7159ba6d967e002891a
Markus Waas <mail@markuswaas.com>
2023-09-26 08:47:43 -0400

    feat: add generic market trait

--- packages/injective-cosmwasm/src/exchange/market.rs
+++ packages/injective-cosmwasm/src/exchange/market.rs
@@ -1,5 +1,8 @@
+use injective_math::FPDecimal;
 use schemars::JsonSchema;
 use serde_repr::{Deserialize_repr, Serialize_repr};
+
+use crate::MarketId;
 
 #[derive(Serialize_repr, Deserialize_repr, Default, Clone, Debug, PartialEq, Eq, JsonSchema, Copy)]
 #[repr(i32)]
@@ -10,4 +13,15 @@ pub enum MarketStatus {
     Paused = 2,
     Demolished = 3,
     Expired = 4,
+}
+
+pub trait GenericMarket {
+    fn get_ticker(&self) -> &str;
+    fn get_quote_denom(&self) -> &str;
+    fn get_maker_fee_rate(&self) -> FPDecimal;
+    fn get_taker_fee_rate(&self) -> FPDecimal;
+    fn get_market_id(&self) -> &MarketId;
+    fn get_status(&self) -> MarketStatus;
+    fn get_min_price_tick_size(&self) -> FPDecimal;
+    fn min_quantity_tick_size(&self) -> FPDecimal;
 }
