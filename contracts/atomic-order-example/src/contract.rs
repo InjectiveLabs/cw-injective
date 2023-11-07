0fa9fa33a9f09317ebb9b38ed1d791cf893170c9
Markus Waas <mail@markuswaas.com>
2023-11-07 13:56:34 -0600

    feat: add injective cosmwasm mock contract

--- contracts/atomic-order-example/src/contract.rs
+++ contracts/atomic-order-example/src/contract.rs
@@ -31,7 +31,7 @@ pub fn instantiate(
     info: MessageInfo,
     msg: InstantiateMsg,
 ) -> Result<Response<InjectiveMsgWrapper>, ContractError> {
-    let querier = InjectiveQuerier::new(&deps.querier);
+    let querier: InjectiveQuerier<'_> = InjectiveQuerier::new(&deps.querier);
     if let Some(market) = querier.query_spot_market(&msg.market_id)?.market {
         let state = ContractConfigState {
             market_id: msg.market_id,
---
+++ contracts/injective-cosmwasm-mock/src/contract.rs
@@ -0,0 +1,41 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cw2::set_contract_version;
+
+use injective_cosmwasm::{create_deposit_msg, InjectiveMsgWrapper, InjectiveQuerier, InjectiveQueryWrapper};
+
+use crate::error::ContractError;
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+const CONTRACT_NAME: &str = "crates.io:injective:dummy";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(deps: DepsMut, _env: Env, _info: MessageInfo, _msg: InstantiateMsg) -> Result<Response, ContractError> {
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+    Ok(Response::default())
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    _deps: DepsMut<InjectiveQueryWrapper>,
+    env: Env,
+    _info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response<InjectiveMsgWrapper>, ContractError> {
+    match msg {
+        ExecuteMsg::TestDepositMsg { subaccount_id, amount } => {
+            Ok(Response::new().add_message(create_deposit_msg(env.contract.address, subaccount_id, amount)))
+        }
+    }
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(deps: Deps<InjectiveQueryWrapper>, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    let querier = InjectiveQuerier::new(&deps.querier);
+
+    match msg {
+        QueryMsg::TestSpotMarketQuery { market_id } => to_binary(&querier.query_spot_market(&market_id)?),
+    }
+}
