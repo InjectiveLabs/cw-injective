3cb14e36d8ab8b8d55ae5608a19a5ba84c5dbc6c
Kishan Dhakan <kishandhakan17@gmail.com>
2023-11-13 21:12:13 +0530

    finish instantiate, execute, query

--- contracts/injective-cosmwasm-mock/src/lib.rs
+++ contracts/injective-cosmwasm-mock/src/lib.rs
@@ -1,5 +1,13 @@
 pub mod contract;
 mod error;
 pub mod msg;
+#[cfg(test)]
+mod testing;
 
 pub use crate::error::ContractError;
+pub use test_tube_inj::account::{Account, FeeSetting, NonSigningAccount, SigningAccount};
+pub use test_tube_inj::runner::error::{DecodeError, EncodeError, RunnerError};
+pub use test_tube_inj::runner::result::{ExecuteResponse, RunnerExecuteResult, RunnerResult};
+pub use test_tube_inj::runner::Runner;
+pub use test_tube_inj::{fn_execute, fn_query};
+
--- contracts/injective-cosmwasm-mock/src/testing/query_spot_market_test.rs
+++ contracts/injective-cosmwasm-mock/src/testing/query_spot_market_test.rs
@@ -1,8 +1,7 @@
-use crate::msg::InstantiateMsg;
-use cosmwasm_std::Coin;
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cosmwasm_std::{Coin, Addr, Binary};
+use injective_cosmwasm::{checked_address_to_subaccount_id, MarketId};
 use injective_test_tube::{Account, InjectiveTestApp, Module, Wasm};
-
-use crate::msg::QueryMsg;
 
 #[test]
 #[cfg_attr(not(feature = "integration"), ignore)]
@@ -13,8 +12,8 @@ fn test_instantiation() {
         .init_accounts(&[Coin::new(1_000_000_000_000, "usdt"), Coin::new(1_000_000_000_000, "inj")], 2)
         .unwrap();
 
-    let admin = &accs[0];
-    let new_admin = &accs[1];
+    let seller = &accs[0];
+    let buyer = &accs[1];
 
     // `Wasm` is the module we use to interact with cosmwasm releated logic on the appchain
     // it implements `Module` trait which you will see more later.
@@ -22,5 +21,37 @@ fn test_instantiation() {
 
     // Load compiled wasm bytecode
     let wasm_byte_code = std::fs::read("../../artifacts/injective_cosmwasm_mock-aarch64.wasm").unwrap();
-    let code_id = wasm.store_code(&wasm_byte_code, None, admin).unwrap().data.code_id;
+    let code_id = wasm.store_code(&wasm_byte_code, None, buyer).unwrap().data.code_id;
+
+    // Instantiate contract
+    let contract_address: String = wasm
+        .instantiate(code_id, &InstantiateMsg {}, Some(&seller.address()), Some("mock-contract"), &[], &seller)
+        .unwrap()
+        .data
+        .address;
+
+    // Execute contract
+    let buyer_subaccount_id =
+                checked_address_to_subaccount_id(&Addr::unchecked(buyer.address()), 1u32);
+    let _res = wasm
+        .execute(
+            &contract_address,
+            &ExecuteMsg::TestDepositMsg {
+                subaccount_id: buyer_subaccount_id,
+                amount: Coin::new(100, "usdt"),
+            },
+            &[Coin::new(100, "usdt")],
+            &buyer,
+        )
+        .unwrap();
+
+    // Query contract
+    let _res: Binary = wasm
+        .query(
+            &contract_address,
+            &QueryMsg::TestSpotMarketQuery {
+                market_id: MarketId::unchecked("0x01edfab47f124748dc89998eb33144af734484ba07099014594321729a0ca16b"),
+            },
+        )
+        .unwrap();
 }
