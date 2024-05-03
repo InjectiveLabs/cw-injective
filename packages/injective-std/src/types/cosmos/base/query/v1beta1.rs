db25f6aa42ae16bae3cf2b317892a9a53513f67c
maxrobot <max.grabner@staedter.io>
2024-05-03 09:53:32 +0100

    fixed mysterious errors

--- packages/injective-std/src/types/cosmos/base/query/v1beta1.rs
+++ packages/injective-std/src/types/cosmos/base/query/v1beta1.rs
@@ -8,7 +8,7 @@ use osmosis_std_derive::CosmwasmExt;
 ///   }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, Eq, ::prost::Message, ::serde::Serialize, ::serde::Deserialize, ::schemars::JsonSchema, CosmwasmExt)]
-#[proto_message(type_url = "/cosmos.base.query.v1beta1.PageRequestPageRequestPageRequest")]
+#[proto_message(type_url = "/cosmos.base.query.v1beta1.PageRequest")]
 pub struct PageRequest {
     /// key is a value returned in PageResponse.next_key to begin
     /// querying the next page most efficiently. Only one of offset or key
@@ -53,7 +53,7 @@ pub struct PageRequest {
 ///   }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, Eq, ::prost::Message, ::serde::Serialize, ::serde::Deserialize, ::schemars::JsonSchema, CosmwasmExt)]
-#[proto_message(type_url = "/cosmos.base.query.v1beta1.PageResponsePageResponsePageResponse")]
+#[proto_message(type_url = "/cosmos.base.query.v1beta1.PageResponse")]
 pub struct PageResponse {
     /// next_key is the key to be passed to PageRequest.key to
     /// query the next page most efficiently. It will be empty if
