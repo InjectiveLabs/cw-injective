d2427b807c639e0f7abcc988e3d673825d047c53
maxrobot <max.grabner@staedter.io>
2024-07-22 15:56:00 +0100

    chore: bumped package to include both permissions and peggy messages

--- packages/injective-std/src/types/injective/exchange/v1beta1.rs
+++ packages/injective-std/src/types/injective/exchange/v1beta1.rs
@@ -1863,9 +1863,6 @@ pub struct MsgInstantSpotMarketLaunch {
     /// quantity
     #[prost(string, tag = "6")]
     pub min_quantity_tick_size: ::prost::alloc::string::String,
-
-    #[prost(string, tag = "7")]
-    pub min_notional: ::prost::alloc::string::String,
 }
 /// MsgInstantSpotMarketLaunchResponse defines the Msg/InstantSpotMarketLaunch
 /// response type.
@@ -1931,9 +1928,6 @@ pub struct MsgInstantPerpetualMarketLaunch {
     /// quantity
     #[prost(string, tag = "13")]
     pub min_quantity_tick_size: ::prost::alloc::string::String,
-
-    #[prost(string, tag = "14")]
-    pub min_notional: ::prost::alloc::string::String,
 }
 /// MsgInstantPerpetualMarketLaunchResponse defines the
 /// Msg/InstantPerpetualMarketLaunchResponse response type.
